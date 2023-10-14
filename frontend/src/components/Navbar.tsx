e5ae813c429e753334a2526adfe6b0b11b701815
Liam Wells Ayathan <ayathan21@gmail.com>
2023-10-14 22:56:38 +0800

    added phantom wallet connection

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,108 +13,60 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { RxAvatar } from "react-icons/rx";
-
-import { getUser } from "@/lib/api";
-
 const Navbar: React.FC = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-
   // web3
   const [web3, setWeb3] = useState<any>(null); // Web3 instance
   const [account, setAccount] = useState<string | null>(null); // User's Ethereum account
 
   // Dropdown state
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [pubKey, setPubKey] = useState<string | null>(null);
 
-  const handleNavigateToProfile = (e: React.MouseEvent) => {
-    router.push("/profile");
-  };
-
-  const handleLogout = (e: React.MouseEvent) => {
-    e.preventDefault();
-    signOut();
-  };
-
-  const handleNavigateToCreateCampaign = () => {
-    router.push("/campaign");
-  };
-
-  const initWeb3 = async () => {
-    if (window.ethereum) {
-      try {
-        // Request access to MetaMask wallet
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-        const web3Instance = new Web3(window.ethereum);
-        setWeb3(web3Instance);
-        console.log("Web3 instance initialized.");
-
-        // Get the user's Ethereum account
-        const accounts = await web3Instance.eth.getAccounts();
-        if (accounts.length > 0) {
-          setAccount(accounts[0]);
+  const getProvider = (isConnected: boolean) => {
+    if ("phantom" in window) {
+      const provider = window.phantom?.solana;
+      if (isConnected) {
+        provider.on("connect", () => console.log("Wallet is connected!"));
+        if (provider?.isPhantom) {
+          return provider;
         }
-      } catch (error) {
-        console.error("Error connecting to MetaMask:", error);
+      } else {
+        provider.on("disconnect", () => console.log("Wallet is disconnected!"));
+        if (provider?.isPhantom) {
+          return provider;
+        }
       }
-    } else {
-      console.error("MetaMask extension not detected.");
     }
+
+    window.open("https://phantom.app/", "_blank");
   };
 
-  useEffect(() => {
-    const fetchUser = async (email: string) => {
-      const user = await getUser(email);
-      console.log("FETCHED USER!!!!!!!!!!");
-      console.log(user);
-
-      if (!user.isKyc) {
-        router.push({
-          pathname: "kyc",
-          query: { email },
-        });
-      }
-    };
-
-    if (session && session.user && session.user.email) {
-      fetchUser(session.user.email);
-    }
-  }, [session]);
-
-  // Handle MetaMask connection
-  const handleConnectMetaMask = async () => {
-    if (!web3) {
-      console.error("Web3 instance not initialized.");
-      initWeb3();
-      return;
-    }
-
+  const establishConnection = async () => {
+    const provider = getProvider(true); // see "Detecting the Provider", adding in true for connected
     try {
-      // Request access to MetaMask wallet
-      await window.ethereum.request({ method: "eth_requestAccounts" });
-
-      // Get the user's Ethereum account
-      const accounts = await web3.eth.getAccounts();
-      if (accounts.length > 0) {
-        setAccount(accounts[0]);
-      }
-    } catch (error) {
-      console.error("Error connecting to MetaMask:", error);
+      const resp = await provider.connect({ onlyIfTrusted: true });
+      console.log(`I connected my wallet ${resp.publicKey.toString()}`);
+      // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo
+      console.log(provider.isConnected);
+      setPubKey(resp.publicKey.toString());
+    } catch (err) {
+      // { code: 4001, message: 'User rejected the request.' }
+      console.log(err);
     }
   };
 
-  // Toggle dropdown
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  // Handle disconnecting MetaMask
-  const handleDisconnectMetaMask = () => {
-    // Disconnect logic here (e.g., reset account and web3)
-    setAccount(null);
-    setWeb3(null);
-    setIsDropdownOpen(false);
+  const handleDisconnect = async () => {
+    const provider = getProvider(false); // see "Detecting the Provider"
+    try {
+      const resp = await provider.disconnect();
+      console.log(`I disconnected my wallet`);
+      // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo
+      console.log(provider.isConnected);
+      setPubKey(null);
+    } catch (err) {
+      // { code: 4001, message: 'User rejected the request.' }
+      console.log(err);
+    }
   };
 
   return (
@@ -123,57 +75,19 @@ const Navbar: React.FC = () => {
         <Link href="/">Solmatch</Link>
       </div>
       <div className="flex space-x-2">
-        {account ? ( // removed && !session
+        {pubKey ? (
           <div className="relative">
-            <Button variant="outline" onClick={toggleDropdown}>
-              Connected: {account.substring(0, 5)}...
+            <Button variant="outline">
+              Connected: {pubKey.substring(0, 5)}...
+            </Button>{" "}
+            <Button variant="outline" onClick={handleDisconnect}>
+              Disconnect
             </Button>
-            {isDropdownOpen && (
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <div
-                    className="absolute inset-0 bg-transparent z-10"
-                    onClick={() => setIsDropdownOpen(false)}
-                  ></div>
-                  <DropdownMenuContent>
-                    <DropdownMenuItem onClick={handleDisconnectMetaMask}>
-                      Disconnect Wallet
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenuTrigger>
-              </DropdownMenu>
-            )}
           </div>
         ) : (
-          !session && (
-            <Button variant="outline" onClick={handleConnectMetaMask}>
-              Connect Wallet
-            </Button>
-          )
-        )}
-        {session ? ( // can delete or augment this since it is not needed anymore -> no charity login
-          <div className="flex items-center gap-2">
-            <Button onClick={handleNavigateToCreateCampaign}>
-              Create a Campaign
-            </Button>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Button variant="outline" size="icon">
-                  <RxAvatar className="h-6 w-6" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem onClick={handleNavigateToProfile}>
-                  Campaigns
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={handleLogout}>
-                  Logout
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        ) : (
-          <p></p>
+          <Button variant="outline" onClick={establishConnection}>
+            Connect Wallet
+          </Button>
         )}
       </div>
     </div>
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({
   Component,
@@ -8,9 +8,7 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-
       <Component {...pageProps} />
-      
     </SessionProvider>
   );
 }
